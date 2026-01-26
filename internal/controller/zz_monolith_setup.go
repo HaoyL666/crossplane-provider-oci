55d6616ffd856a19dd7c4ff6deb4b14afec2add3
Abhilash Reddy Endurthi <abhilash.reddy.endurthi@oracle.com>
2026-01-26 13:57:22 -0800

    Remove alias resource for loadbalancer backendset

--- internal/controller/zz_monolith_setup.go
+++ internal/controller/zz_monolith_setup.go
@@ -135,7 +135,6 @@ import (
 	vault "github.com/oracle/provider-oci/internal/controller/kms/vault"
 	backend "github.com/oracle/provider-oci/internal/controller/loadbalancer/backend"
 	backendset "github.com/oracle/provider-oci/internal/controller/loadbalancer/backendset"
-	balancerbackendset "github.com/oracle/provider-oci/internal/controller/loadbalancer/balancerbackendset"
 	certificate "github.com/oracle/provider-oci/internal/controller/loadbalancer/certificate"
 	lbhostname "github.com/oracle/provider-oci/internal/controller/loadbalancer/lbhostname"
 	listener "github.com/oracle/provider-oci/internal/controller/loadbalancer/listener"
@@ -356,7 +355,6 @@ func Setup_monolith(mgr ctrl.Manager, o controller.Options) error {
 		vault.Setup,
 		backend.Setup,
 		backendset.Setup,
-		balancerbackendset.Setup,
 		certificate.Setup,
 		lbhostname.Setup,
 		listener.Setup,
